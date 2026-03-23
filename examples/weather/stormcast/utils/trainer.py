d215061e4faea2bf836642e361fef7dfe207e332
root <root@pool0-01755.cm.cluster>
2026-03-23 06:55:51 -0700

    force stormcast to use same sigma_data as loss
    Signed-off-by: root <root@pool0-01755.cm.cluster>

--- examples/weather/stormcast/utils/trainer.py
+++ examples/weather/stormcast/utils/trainer.py
@@ -390,6 +390,14 @@ class Trainer:
 
         # Build network
         model_cfg = self.cfg.model
+        model_hparams = dict(model_cfg.hyperparameters)
+        if self.loss_type != "regression":
+            loss_sigma_data = self.cfg.training.loss.sigma_data
+            model_hparams.setdefault("sigma_data", loss_sigma_data)
+            self.logger.info(
+                f"Preconditioner sigma_data: {model_hparams['sigma_data']} "
+                f"(loss sigma_data: {loss_sigma_data})"
+            )
         if model_cfg.architecture == "unet":
             net = get_preconditioned_unet(
                 name=self.net_name,
@@ -399,7 +407,7 @@ class Trainer:
                 lead_time_steps=self.lead_time_steps,
                 amp_mode=self.enable_amp,
                 use_apex_gn=self.use_apex_gn,
-                **model_cfg.hyperparameters,
+                **model_hparams,
             )
         elif model_cfg.architecture == "dit":
             net = get_preconditioned_natten_dit(
@@ -408,7 +416,7 @@ class Trainer:
                 conditional_channels=num_condition_channels,
                 scalar_condition_channels=len(self.scalar_cond_channels),
                 lead_time_steps=self.lead_time_steps,
-                **model_cfg.hyperparameters,
+                **model_hparams,
             )
         else:
             raise ValueError("model.architecture must be 'unet' or 'dit'")
